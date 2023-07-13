b1cdc7facd669730f5cd37c3207496c97bd1207d
samuelho-dev <samuelho343@gmail.com>
2023-07-13 00:49:11 -0700

    removed placeholder data

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,114 +1,6 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
-const data = [
-  {
-    country: "AD",
-    "hot dog": 23,
-    "hot dogColor": "hsl(65, 70%, 50%)",
-    burger: 194,
-    burgerColor: "hsl(65, 70%, 50%)",
-    sandwich: 58,
-    sandwichColor: "hsl(104, 70%, 50%)",
-    kebab: 157,
-    kebabColor: "hsl(159, 70%, 50%)",
-    fries: 7,
-    friesColor: "hsl(324, 70%, 50%)",
-    donut: 70,
-    donutColor: "hsl(144, 70%, 50%)",
-  },
-  {
-    country: "AE",
-    "hot dog": 15,
-    "hot dogColor": "hsl(146, 70%, 50%)",
-    burger: 105,
-    burgerColor: "hsl(29, 70%, 50%)",
-    sandwich: 54,
-    sandwichColor: "hsl(123, 70%, 50%)",
-    kebab: 150,
-    kebabColor: "hsl(33, 70%, 50%)",
-    fries: 152,
-    friesColor: "hsl(180, 70%, 50%)",
-    donut: 102,
-    donutColor: "hsl(5, 70%, 50%)",
-  },
-  {
-    country: "AF",
-    "hot dog": 63,
-    "hot dogColor": "hsl(147, 70%, 50%)",
-    burger: 53,
-    burgerColor: "hsl(35, 70%, 50%)",
-    sandwich: 47,
-    sandwichColor: "hsl(190, 70%, 50%)",
-    kebab: 142,
-    kebabColor: "hsl(334, 70%, 50%)",
-    fries: 57,
-    friesColor: "hsl(359, 70%, 50%)",
-    donut: 101,
-    donutColor: "hsl(128, 70%, 50%)",
-  },
-  {
-    country: "AG",
-    "hot dog": 162,
-    "hot dogColor": "hsl(67, 70%, 50%)",
-    burger: 134,
-    burgerColor: "hsl(16, 70%, 50%)",
-    sandwich: 161,
-    sandwichColor: "hsl(188, 70%, 50%)",
-    kebab: 139,
-    kebabColor: "hsl(129, 70%, 50%)",
-    fries: 128,
-    friesColor: "hsl(174, 70%, 50%)",
-    donut: 84,
-    donutColor: "hsl(128, 70%, 50%)",
-  },
-  {
-    country: "AI",
-    "hot dog": 166,
-    "hot dogColor": "hsl(55, 70%, 50%)",
-    burger: 190,
-    burgerColor: "hsl(316, 70%, 50%)",
-    sandwich: 112,
-    sandwichColor: "hsl(70, 70%, 50%)",
-    kebab: 170,
-    kebabColor: "hsl(202, 70%, 50%)",
-    fries: 21,
-    friesColor: "hsl(356, 70%, 50%)",
-    donut: 4,
-    donutColor: "hsl(133, 70%, 50%)",
-  },
-  {
-    country: "AL",
-    "hot dog": 127,
-    "hot dogColor": "hsl(304, 70%, 50%)",
-    burger: 3,
-    burgerColor: "hsl(311, 70%, 50%)",
-    sandwich: 86,
-    sandwichColor: "hsl(301, 70%, 50%)",
-    kebab: 33,
-    kebabColor: "hsl(11, 70%, 50%)",
-    fries: 77,
-    friesColor: "hsl(203, 70%, 50%)",
-    donut: 83,
-    donutColor: "hsl(9, 70%, 50%)",
-  },
-  {
-    country: "AM",
-    "hot dog": 24,
-    "hot dogColor": "hsl(92, 70%, 50%)",
-    burger: 75,
-    burgerColor: "hsl(282, 70%, 50%)",
-    sandwich: 3,
-    sandwichColor: "hsl(212, 70%, 50%)",
-    kebab: 109,
-    kebabColor: "hsl(126, 70%, 50%)",
-    fries: 111,
-    friesColor: "hsl(283, 70%, 50%)",
-    donut: 131,
-    donutColor: "hsl(175, 70%, 50%)",
-  },
-];
-
 const ResponsiveBarChart = dynamic(
   () => import("@nivo/bar").then((m) => m.ResponsiveBar),
   { ssr: false }
@@ -222,15 +114,23 @@ const theme = {
   },
 };
 
-const BarChart = () => (
+const BarChart = ({ data }) => (
   <ResponsiveBarChart
     data={data}
     theme={theme}
-    keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
-    indexBy="country"
+    keys={[
+      "Only English",
+      "Spanish",
+      "Other Indo-European Languages",
+      "Asian and Pacific Island Languages",
+      "Other Languages",
+    ]}
+    enableGridY={false}
+    indexBy="state"
+    groupMode="grouped"
     margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
     padding={0.3}
-    valueScale={{ type: "linear" }}
+    valueScale={{ type: "symlog" }}
     indexScale={{ type: "band", round: true }}
     colors={{ scheme: "nivo" }}
     defs={[
@@ -285,9 +185,9 @@ const BarChart = () => (
       tickSize: 5,
       tickPadding: 5,
       tickRotation: 0,
-      legend: "food",
+      legend: "Languages Spoken by Population",
       legendPosition: "middle",
-      legendOffset: -40,
+      legendOffset: -50,
     }}
     labelSkipWidth={12}
     labelSkipHeight={12}
@@ -298,13 +198,13 @@ const BarChart = () => (
     legends={[
       {
         dataFrom: "keys",
-        anchor: "bottom-right",
+        anchor: "top-right",
         direction: "column",
         justify: false,
-        translateX: 120,
+        translateX: 250,
         translateY: 0,
         itemsSpacing: 2,
-        itemWidth: 100,
+        itemWidth: 300,
         itemHeight: 20,
         itemDirection: "left-to-right",
         itemOpacity: 0.85,
@@ -320,7 +220,6 @@ const BarChart = () => (
       },
     ]}
     role="application"
-    ariaLabel="Nivo bar chart demo"
   />
 );
 
