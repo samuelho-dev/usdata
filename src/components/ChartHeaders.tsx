606f5ce987c6ed26cad19f996ed246dc87d1a32c
samuelho-dev <samuelho343@gmail.com>
2023-07-15 00:28:47 -0700

    chart headers

---
+++ src/components/ChartHeaders.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface ChartHeadersInterface {
+  [key: string]: string;
+}
+
+function ChartHeaders({ variables }: { variables: ChartHeadersInterface }) {
+  return (
+    <div>
+      <div>
+        <h5 className="text-lg text-white">Variables Used</h5>
+        {Object.keys(variables).map((chartVar, i) => (
+          <p className="text-sm text-white" key={i}>
+            {`${chartVar} : ${variables[chartVar] as string}`}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ChartHeaders;
