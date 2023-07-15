fddac59339bf009522b7f828c0585b20491db097
samuelho-dev <samuelho343@gmail.com>
2023-07-14 21:28:38 -0700

    add bump mutation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import BarChart from "~/components/Bar";
+import BumpChart from "~/components/Bump";
 import LineChart from "~/components/Line";
 import RadarChart from "~/components/Radar";
 
 import {
+  type BumpDataSchema,
   type BarDataSchema,
   type LineDataSchema,
   type RadarDataSchema,
@@ -16,6 +18,7 @@ export default function Home() {
   const radarDataMutation = api.fetch.fetchRadarData.useMutation();
   const lineDataMutation = api.fetch.fetchLineData.useMutation();
   const barDataMutation = api.fetch.fetchBarData.useMutation();
+  const bumpDataMutation = api.fetch.fetchBumpData.useMutation();
 
   const [selectedDate, setSelectedDate] = useState(2005);
   const [selectedStates, setSelectedStates] = useState<
@@ -25,6 +28,7 @@ export default function Home() {
   const [radarData, setRadarData] = useState<RadarDataSchema[] | null>(null);
   const [lineData, setLineData] = useState<LineDataSchema[] | null>(null);
   const [barData, setBarData] = useState<BarDataSchema[] | null>(null);
+  const [bumpData, setBumpData] = useState<BumpDataSchema[] | null>(null);
 
   const handleStateSelection = (FIPS: string, state: string) => {
     setSelectedStates((prevState) => {
@@ -38,7 +42,7 @@ export default function Home() {
   };
 
   const handleDataMutation = async () => {
-    // Grab the associated models related to the states & year
+    // Grab the associated models related to the states & year for Census Data
     const dataModels = await datasetMutation.mutateAsync({
       year: selectedDate,
       ids: selectedStates.map((state) => state.FIPS),
@@ -52,6 +56,11 @@ export default function Home() {
       selectedStates.map((el) => el.FIPS)
     );
     setLineData(lineQueryData);
+
+    const bumpQueryData = await bumpDataMutation.mutateAsync(
+      selectedStates.map((el) => el.FIPS)
+    );
+    setBumpData(bumpQueryData);
 
     const barQueryData = await barDataMutation.mutateAsync(dataModels);
     setBarData(barQueryData);
@@ -105,11 +114,15 @@ export default function Home() {
           </button>
         </div>
         <div className="grid w-full  grid-cols-2 items-center justify-center gap-2">
-          <div className="col-span-2 h-80">
+          <div className="col-span-2 flex h-80 flex-col justify-center">
+            <h2 className="text-xl text-purple-400">KPI</h2>
             {radarData && <RadarChart data={radarData} />}
           </div>
           <div className="h-80">
             {lineData && <LineChart data={lineData} />}
+          </div>
+          <div className="h-80">
+            {bumpData && <BumpChart data={bumpData} />}
           </div>
           <div className="col-span-2 h-80">
             {barData && <BarChart data={barData} />}
