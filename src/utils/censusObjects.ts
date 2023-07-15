ab9fa1e0d532408df9dfc4aaef925909500d0d35
samuelho-dev <samuelho343@gmail.com>
2023-07-15 00:28:32 -0700

    census obj types

---
+++ src/utils/censusObjects.ts
@@ -0,0 +1,23 @@
+type Variables = {
+  [key: string]: string;
+};
+
+export const radarVariables: Variables = {
+  B19013_001E: `Adjusted Past Year Median Household Income`,
+  B25003_002E: `Owner Occupied Housing Units`,
+  B25064_001E: `Median Gross Rent`,
+  B15003_022E: `Bachelor's Degree or Higher`,
+};
+
+export const lineVariables = {
+  B25001_001E: `Total Housing Units`,
+};
+
+export const barVariables: Variables = {
+  B01003_001E: `Total Population`,
+  B16001_002E: `Only English`,
+  B16001_003E: `Spanish`,
+  B16001_006E: `Other Indo-European`,
+  B16001_009E: `Asian and Pacific Island`,
+  B16001_012E: `Other`,
+};
