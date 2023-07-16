811a3a070b8ecb7fae7aad798a19e3ad62dda3ac
samuelho-dev <samuelho343@gmail.com>
2023-07-15 20:48:43 -0700

    styling

--- src/components/ChartHeaders.tsx
+++ src/components/ChartHeaders.tsx
@@ -7,15 +7,13 @@ interface ChartHeadersInterface {
 function ChartHeaders({ variables }: { variables: ChartHeadersInterface }) {
   return (
     <div>
-      <div>
-        <h5 className="text-lg text-white">Variables Used</h5>
-        {Object.keys(variables).map((chartVar, i) => (
-          <p className="text-sm text-white" key={i}>
-            <span className="text-[#FE704E]">{chartVar}</span>
-            {` : ${variables[chartVar] as string}`}
-          </p>
-        ))}
-      </div>
+      <h5 className="text-lg text-white">Variables Used</h5>
+      {Object.keys(variables).map((chartVar, i) => (
+        <p className="text-sm text-white" key={i}>
+          <span className="text-[#FE704E]">{chartVar}</span>
+          {` : ${variables[chartVar] as string}`}
+        </p>
+      ))}
     </div>
   );
 }
